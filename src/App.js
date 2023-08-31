3594713f9722d3511782f645e82d07a525a6aaba
Марина Мукобенова <marinamukobenova@MacBook-Air.lan>
2023-08-30 20:38:49 -0400

    changed

--- src/App.js
+++ src/App.js
@@ -4,50 +4,49 @@ import { data } from './data';
 
 
 function App() {
-const [list, setList] = useState(0);
-const {id, thing, pic} = data[list];
-const [button, setButton] = useState(data)
+const [list, setList] = useState(data);
+const [index, setIndex] = useState(0)
+const {id, thing, pic} = list[index];
+
 
 
 const previousTask = () =>{
-setList ((list =>{
-list --;
-if(list < 0){
-return data.length -1;
+setIndex ((index =>{
+index --;
+if(index < 0){
+return list.length -1;
 }
-return list;
+return index;
 }))
 }
 
 const nextTask = () =>{
-setList ((list =>{
-list ++;
-if (list > data.length - 1){
-list = 0;
+setIndex ((index =>{
+index ++;
+if (index > list.length - 1){
+index = 0;
 }
-return list;
+return index;
 }))
 }
 
 const markCompleted = (id) =>{
-   console.log(id)
-let goalsCompleted = data.filter(data => data.id !== data.id);
-console.log(goalsCompleted)
-setButton(goalsCompleted)
+let goalsCompleted = list.filter(list => list.id !== id);
+setList(goalsCompleted)
 } 
 
 return (
 <div>
    <div className='container'>
-      <h1>List of { data.length } things I want to do within the next 5 years</h1>
+      <h1>List of { list.length } things I want to do within the next 5 years</h1>
    </div>
-   <div className='container' key={id}>
+   <div className='container'>
       <h2>{ id } - { thing } </h2>
    </div>
    <div className='container'>
-      <button className='btn' onClick={(previousTask)}>Previous</button>
+      <button className='btn' onClick={previousTask}>Previous</button>
       <img src={ pic }  width='400px' height='400px' alt="pic" />
-      <button className='btn' onClick={(nextTask)}>Next</button>
+      <button className='btn' onClick={nextTask}>Next</button>
    </div>
 <div className='container'>
    <button className='completed' onClick={()=>markCompleted(id)}>Completed</button>
@@ -57,29 +56,4 @@ return (
 )
 }
 
-/*function App(){
-   const [list, setList] = useState(data);
-   console.log(list);
-
-
-
-   return(
-      <div>
-         <div className='container'>
-      <h1>List of { data.length } things I want to do within the next 5 years</h1>
-   </div>
-   {list.map((element => {
-      const {id, thing, image} = element;
-
-      return( 
-         <div>
-            <div className='container' key={id}>
-      <h2>{ id } - { thing } </h2>
-   </div>
-         </div>
-      )
-   }))}
-      </div>
-   )
-}*/
 export default App;
